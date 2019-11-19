ae23666bc5a0ccee540a5c89d7ef8ceafd5346b9
zhairp <zhairp@zhairpdeMacBook-Pro.local>
2019-11-19 16:43:41 +0800

    eureka init

---
+++ client/src/main/java/eurekaclientdemo/EurekaClientApplication.java
@@ -0,0 +1,18 @@
+package eurekaclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Gunnar Hillert
+ *
+ */
+@SpringBootApplication
+public class EurekaClientApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+
+}
